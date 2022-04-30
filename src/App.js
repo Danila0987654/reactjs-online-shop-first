bb5075b93d49540eb787a0e3a62c82269d16da29
Соколов Данила <2000akula@mail.ru>
2022-04-30 10:54:42 +0300

    Change function on class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-function App() {
-  return (
-      <div className="wrapper">
-        <Header/>
-        <Footer/>
-      </div>
-  );
+class App extends React.Component{
+    render() {
+        return (
+            <div className="wrapper">
+                <Header/>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
